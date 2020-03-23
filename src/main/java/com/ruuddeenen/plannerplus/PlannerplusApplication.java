a045749561e26761fce83965bf0513ce8dc4c10b
R.C.H. Deenen <r.deenen@fontys.student.nl>
2020-03-23 15:27:19 +0100

    Initial project

---
+++ src/main/java/com/ruuddeenen/plannerplus/PlannerplusApplication.java
@@ -0,0 +1,13 @@
+package com.ruuddeenen.plannerplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruuddeenen/plannerplus/PlannerplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruuddeenen.plannerplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
